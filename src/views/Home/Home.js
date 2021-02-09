6533b348543bd6fdc0f06cd684081ea635a379ba
camilarodriguez <kmivivi12@gmail.com>
2021-02-09 14:54:01 -0800

    SpotNews change

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -41,6 +41,7 @@ const Home = () => {
               longitudeDelta: 0.0121,
             }}>
             <Marker
+            // commit for change
               coordinate={{latitude: 37.78825, longitude: -122.4324}}
               image={require('../../images/spotPin.png')}
             />
