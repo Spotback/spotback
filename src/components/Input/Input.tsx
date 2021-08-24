e7f81527c9445bc3409f4fbc56012eff150c2006
Joseph Caruana <jcaruana614@gmail.com>
2021-08-23 20:43:57 -0500

    initial implementation

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,18 +3,20 @@ import { TextInput } from 'react-native';
 import styles from './Input.styles';
 
 interface Inputprops {
-  placeholder?: string;
+  placeholder?: any;
   inputStyle: string;
+  onBlur?: any;
+  onChangeText?: any;
+  value?: any;
 }
-const Input: FC<Inputprops> = ({ placeholder, inputStyle }) => {
-  const [value, onChangeText] = useState('');
-
+const Input: FC<Inputprops> = ({ placeholder, inputStyle, onBlur, onChangeText, value }) => {
   return (
     <TextInput
-      onChangeText={(text) => onChangeText(text)}
+      onChangeText={onChangeText}
       value={value}
       placeholder={placeholder}
       style={inputStyle === 'long' ? styles.longInput : styles.smallInput}
+      onBlur={onBlur}
     />
   );
 };
--- src/views/Signup/Signup.tsx
+++ src/views/Signup/Signup.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text, View, TextInput, Alert } from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
 import { useNavigation } from '@react-navigation/native';
 import Button from '../../components/Button/Button';
 import Input from '../../components/Input/Input';
@@ -7,26 +8,131 @@ import styles from './Signup.styles';
 
 const Signup = () => {
   const navigation = useNavigation();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log('data', data);
 
   return (
     <View style={styles.container}>
-      <Input placeholder="Your Email" inputStyle="long" />
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <Input
+            placeholder={errors.email ? 'This is required.' : 'Your Email'}
+            inputStyle="long"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="email"
+        defaultValue=""
+      />
+
       <View style={styles.centerContainer}>
-        <View style={styles.input}>
-          <Input placeholder="First Name" inputStyle="short" />
-        </View>
-        <View style={styles.input}>
-          <Input placeholder="Last Name" inputStyle="short" />
-        </View>
-        <View style={styles.input}>
-          <Input placeholder="Phone Number" inputStyle="short" />
-        </View>
-        <View style={styles.input}>
-          <Input placeholder="Password" inputStyle="short" />
-        </View>
-        <Input placeholder="Referral Code" inputStyle="long" />
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View style={styles.input}>
+              <Input
+                placeholder={errors.firstName ? 'This is required.' : 'First Name'}
+                inputStyle="short"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            </View>
+          )}
+          name="firstName"
+          defaultValue=""
+        />
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View style={styles.input}>
+              <Input
+                placeholder={errors.lastName ? 'This is required.' : 'Last Name'}
+                inputStyle="short"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            </View>
+          )}
+          name="lastName"
+          defaultValue=""
+        />
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View style={styles.input}>
+              <Input
+                placeholder={errors.phoneNumber ? 'This is required.' : 'Phone Number'}
+                inputStyle="short"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            </View>
+          )}
+          name="phoneNumber"
+          defaultValue=""
+        />
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View style={styles.input}>
+              <Input
+                placeholder={errors.password ? 'This is required.' : 'Password'}
+                inputStyle="short"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            </View>
+          )}
+          name="password"
+          defaultValue=""
+        />
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View style={styles.input}>
+              <Input
+                placeholder={errors.referralCode ? 'This is required.' : 'Referral Code'}
+                inputStyle="long"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            </View>
+          )}
+          name="referralCode"
+          defaultValue=""
+        />
         <View style={styles.button}>
-          <Button title="Sign Up" size="large" onPress={() => navigation.navigate('Home')} />
+          <Button title="Sign Up" size="large" onPress={handleSubmit(onSubmit)} />
         </View>
       </View>
     </View>
