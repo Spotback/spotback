9e603833650a4764b0d850f289d7fae2841c9b8b
Joseph Caruana <jcaruana614@gmail.com>
2021-09-11 17:49:26 -0500

    firebase connection succseful

--- src/views/EditProfile/EditProfile.tsx
+++ src/views/EditProfile/EditProfile.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
-import { View, Text, Image } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, TouchableOpacity, Alert } from 'react-native';
+import { useSelector, RootStateOrAny } from 'react-redux';
 import { useDispatch } from 'react-redux';
+import { launchImageLibrary } from 'react-native-image-picker';
+import storage from '@react-native-firebase/storage';
 import { useForm, Controller } from 'react-hook-form';
 import { Button, Input } from '@components/index';
 import { editProfile, profilePic } from '@assets/images/index';
@@ -8,6 +11,10 @@ import useStyles from './EditProfile.styles';
 
 const EditProfile = () => {
   const styles = useStyles();
+  const user = useSelector((state: RootStateOrAny) => state.userReducer);
+  console.log('userSelector ', user);
+  const [imageSource, setImageSource] = useState({});
+
   const dispatch = useDispatch();
   const {
     control,
@@ -15,10 +22,48 @@ const EditProfile = () => {
     formState: { errors },
   } = useForm();
 
+  const selectImage = () => {
+    const options: any = {
+      title: 'Pick a new profile pick',
+      maxWidth: 256,
+      maxHeight: 256,
+      noData: true,
+      mediaType: 'photo',
+      storageOptions: {
+        skipBackup: true,
+      },
+    };
+
+    launchImageLibrary(options, (response: any) => {
+      if (response.didCancel) {
+        console.log('User cancelled photo picker');
+        Alert.alert('You did not select any image');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        const image = response.assets[0].uri;
+        setImageSource(image);
+        console.log('the uri ', image);
+        storage()
+          .ref(`users/profile_images/${user.email.replace('@', '_').replace('.', '_')}.png`)
+          .putFile(image)
+          .then((snapshot) => {
+            console.log(`${image} has been successfully uploaded.`);
+          })
+          .catch((e) => console.log('uploading image error => ', e));
+      }
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Image style={styles.image} source={editProfile} />
-      <Image style={styles.profilePicImage} source={profilePic} />
+      <TouchableOpacity onPress={selectImage}>
+        <Image style={styles.profilePicImage} source={profilePic} />
+      </TouchableOpacity>
+
       <View style={styles.centerContainer}>
         <View style={styles.itemContainer}>
           <Text style={styles.subText}>Email: </Text>
