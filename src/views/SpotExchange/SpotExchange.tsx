f5571fb27c29d96971678940e416448ca446a786
Joseph Caruana <jcaruana614@gmail.com>
2021-11-30 20:52:31 -0600

    modal inception

--- src/views/SpotExchange/SpotExchange.tsx
+++ src/views/SpotExchange/SpotExchange.tsx
@@ -1,14 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import React, { useState, useEffect } from 'react';
-import {
-  View,
-  Text,
-  Image,
-  Modal,
-  ScrollView,
-  TouchableOpacity,
-  TextInput,
-} from 'react-native';
+import { View, Text, Image, Modal, ScrollView, TouchableOpacity, TextInput } from 'react-native';
 import { useSelector, RootStateOrAny } from 'react-redux';
 import { Button, Hub, Stars } from '@components/index';
 import { useNavigation } from '@react-navigation/native';
@@ -22,7 +14,8 @@ import { theme } from '@utils/theme';
 const SpotExchange = () => {
   const styles = useStyles();
   const navigation = useNavigation();
-  const [modalVisible, setModalVisible] = useState(false);
+  const [modalVis, setModalVis] = useState(false);
+  const [secondaryModalVis, setSecondaryModalVis] = useState(false);
   const [hubVis, setHubVis] = useState(false);
   const user = useSelector((state: RootStateOrAny) => state.userReducer);
   const [imageSource, setImageSource] = useState('');
@@ -48,31 +41,62 @@ const SpotExchange = () => {
     getProfilePic();
   });
 
-  const modalVisability = () => {
-    setModalVisible(!modalVisible);
-    setHubVis(!hubVis);
+  const SecondaryModal = () => {
+    return (
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={secondaryModalVis}
+        onRequestClose={() => {
+          setSecondaryModalVis(!secondaryModalVis);
+        }}>
+        <View style={styles.modalContainer}>
+          <View style={styles.centeredView}>
+            <TouchableOpacity onPress={() => setSecondaryModalVis(!secondaryModalVis)}>
+              <View style={styles.modalView}>
+                <View style={styles.modalTextContainer}>
+                  <Text style={styles.modalText}>
+                    If you cancel during this transaction a fee may apply.
+                  </Text>
+                  <Text style={styles.modalText}>Are you sure you want to cancel?</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+    );
   };
+
   return (
     <View style={styles.mainContainer}>
       <Hub
-        title="Arrving in 5 Minutes"
+        title="Arriving in 5 Minutes"
         client
         imageSource={imageSource}
         balance={15}
-        onPress={() => modalVisability()}
+        onPress={() => {
+          setModalVis(!modalVis);
+          setHubVis(!hubVis);
+        }}
         hide={hubVis}
       />
 
       <Modal
         animationType="fade"
         transparent={true}
-        visible={modalVisible}
+        visible={modalVis}
         onRequestClose={() => {
-          modalVisability();
+          setModalVis(!modalVis);
+          setHubVis(!hubVis);
         }}>
         <View style={styles.modalContainer}>
           <View style={styles.centeredView}>
-            <TouchableOpacity onPress={() => modalVisability()}>
+            <TouchableOpacity
+              onPress={() => {
+                setModalVis(!modalVis);
+                setHubVis(!hubVis);
+              }}>
               <View style={styles.modalView}>
                 <View style={styles.modalTextContainer}>
                   <Text style={styles.modalText}>Walter White</Text>
@@ -91,11 +115,18 @@ const SpotExchange = () => {
               title="Cancel Transaction"
               backgroundColor={theme.colors.light}
               titleColor={theme.colors.error}
-              onPress={() => navigation.navigate('Home')}
+              onPress={() => {
+                setModalVis(!modalVis);
+                setHubVis(!hubVis);
+                setTimeout(() => {
+                  setSecondaryModalVis(!secondaryModalVis);
+                }, 0.7);
+              }}
             />
           </View>
         </View>
       </Modal>
+
       <View style={styles.subContainer}>
         <View style={styles.mapView}>
           <MapView
@@ -109,6 +140,7 @@ const SpotExchange = () => {
             }}
           />
         </View>
+        {SecondaryModal()}
         <View style={styles.messengerContainer}>
           <View style={styles.spotSwitchCompleteContainer}>
             <Button
@@ -125,6 +157,7 @@ const SpotExchange = () => {
               placeholder="Type a message..."
               //   onChangeText={onChangeText} value={value}
             />
+
             <View style={styles.buttonContainer}>
               <Button
                 size="small"
