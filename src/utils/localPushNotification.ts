056d7cc2f223f2f174192db91e27b871b33d690a
Joseph Caruana <jcaruana614@gmail.com>
2022-08-29 20:33:29 -0500

    added some stuff

--- src/utils/localPushNotification.ts
+++ src/utils/localPushNotification.ts
@@ -1,8 +1,39 @@
-import notifee, { AndroidStyle, Notification } from '@notifee/react-native';
+import notifee, { AndroidStyle, AuthorizationStatus, Notification } from '@notifee/react-native';
+import { Alert } from 'react-native';
 
 export const onDisplayNotification = async (id, name, title, body, smallIcon?) => {
   // Request permissions (required for iOS)
   await notifee.requestPermission();
+
+  const batteryOptimizationEnabled = await notifee.isBatteryOptimizationEnabled();
+  if (batteryOptimizationEnabled) {
+    // 2. ask your users to disable the feature
+    Alert.alert(
+      'Restrictions Detected',
+      'To ensure notifications are delivered, please disable battery optimization for the app.',
+      [
+        // 3. launch intent to navigate the user to the appropriate screen
+        {
+          text: 'OK, open settings',
+          onPress: async () => await notifee.openBatteryOptimizationSettings(),
+        },
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+      ],
+      { cancelable: false }
+    );
+  }
+  
+  const settings = await notifee.getNotificationSettings();
+
+  if (settings.authorizationStatus == AuthorizationStatus.AUTHORIZED) {
+    console.log('Notification permissions has been authorized');
+  } else if (settings.authorizationStatus == AuthorizationStatus.DENIED) {
+    console.log('Notification permissions has been denied');
+  }
 
   // Create a channel (required for Android)
   const channelId = await notifee.createChannel({
