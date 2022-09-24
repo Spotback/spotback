e834ccdc6cc3b5ea0372b3e3f11c49bb60fe4797
Joseph Caruana <jcaruana614@gmail.com>
2022-09-24 16:59:59 -0500

    redux persist

--- App.tsx
+++ App.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from './src/services/redux/store';
+import { store, persistor } from './src/services/redux/store';
 import { ThemeProvider } from 'react-native-elements';
 import { theme } from './src/utils/theme';
 import StackNavigator from './src/navigation/StackNavigator';
+import { PersistGate } from 'redux-persist/integration/react';
 // import { StripeProvider } from '@stripe/stripe-react-native';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        {/* <StripeProvider publishableKey="pk_test_q4RuLAlmlO8T8Y2u3jpQwvFZ00KoMvVudh"> */}
-        <StackNavigator />
-        {/* </StripeProvider> */}
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          {/* <StripeProvider publishableKey="pk_test_q4RuLAlmlO8T8Y2u3jpQwvFZ00KoMvVudh"> */}
+          <StackNavigator />
+          {/* </StripeProvider> */}
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 };
--- src/services/redux/store.ts
+++ src/services/redux/store.ts
@@ -1,7 +1,18 @@
 import { applyMiddleware, createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer, applyMiddleware(thunk));
+
+const persistor = persistStore(store);
+
+export { store, persistor };
