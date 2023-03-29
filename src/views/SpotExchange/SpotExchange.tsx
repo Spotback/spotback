4ec3d7797fb5da3fb47dc5674f87d5fb967376d2
camilarodriguez <kmivivi12@gmail.com>
2023-03-29 18:56:44 -0500

    display matched user car picture on the transaction hub

--- src/views/SpotExchange/SpotExchange.tsx
+++ src/views/SpotExchange/SpotExchange.tsx
@@ -1,42 +1,26 @@
 /* eslint-disable @typescript-eslint/camelcase */
 /* eslint-disable react/no-unescaped-entities */
-import { phone, sendMessage, exit } from '@assets/images/index';
-import { Button, Options, Stars } from '@components/index';
+import { Button, Options } from '@components/index';
 import { GOOGLE_API_KEY } from '@env';
 import Polyline from '@mapbox/polyline';
-import database from '@react-native-firebase/database';
 import storage from '@react-native-firebase/storage';
 import { useNavigation } from '@react-navigation/native';
 import { theme } from '@utils/theme';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import {
-  Image,
-  Linking,
-  LogBox,
-  Modal,
-  Platform,
-  ScrollView,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import { LogBox, Modal, Text, View } from 'react-native';
 import { RootStateOrAny, useSelector } from 'react-redux';
 import useStyles from './SpotExchange.styles';
 import MapboxNavigation from '@homee/react-native-mapbox-navigation';
 import { coordinatesSeperator } from '@utils/coordinatesSeperator';
 import { Coordinate } from 'react-native-maps';
-import { useSetTransactionId } from '../../hooks/useSetTransactionId';
+import HubModal from './HubModal';
 
 const SpotExchange = () => {
   const styles = useStyles();
   const navigation = useNavigation();
   const user = useSelector((state: RootStateOrAny) => state.userReducer);
-  const transactionId =   useSetTransactionId();
   const [imageSource, setImageSource] = useState('');
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState({});
   const [userHubModalVis, setuserHubModalVis] = useState(false);
   const [cancelCompleteModalVis, setCancelCompleteModalVis] = useState({
     visible: false,
@@ -55,7 +39,6 @@ const SpotExchange = () => {
   // google maps navigation
   console.log('spotExchange User Data ', user.matchedUsersData.body);
   console.log('spotExchange current and desired location ', currentLocation, desiredLocation);
-  const dbChatRoomRef = database().ref(`chat_rooms/-${transactionId}/messages`);
 
   const getProfilePic = () => {
     storage()
@@ -68,24 +51,6 @@ const SpotExchange = () => {
         setImageSource('');
         console.log('getting downloadURL of image error => ', e);
       });
-  };
-
-  const retrieveTransactionChatRoomMessages = () => {
-    dbChatRoomRef.orderByChild('created').on('value', (messages) => {
-      setMessages(messages.val());
-    });
-  };
-
-  const pushTransactionChatRoomMessage = (messageFromButton) => {
-    const newMessage = dbChatRoomRef.push();
-    newMessage
-      .set({
-        created: Date.now(),
-        sender_id: user.email,
-        text: message === '' ? messageFromButton : message,
-      })
-      .then(() => console.log('Message sent!'));
-    setMessage('');
   };
 
   const getDirections = async (startLoc, destinationLoc) => {
@@ -107,35 +72,6 @@ const SpotExchange = () => {
     }
   };
 
-  const displayMessages = () => {
-    return messages !== null
-      ? Object.values(messages)
-          .sort((a: any, b: any) => b.created - a.created)
-          .map((item: any, index: number) => {
-            return (
-              <View
-                key={index}
-                style={
-                  item.sender_id === user.email ? styles.incomingTextRight : styles.incomingTextLeft
-                }>
-                <Text style={styles.incomingTextMsg}>{item.text}</Text>
-              </View>
-            );
-          })
-      : null;
-  };
-
-  const makeCall = () => {
-    let phoneNumber = '';
-
-    if (Platform.OS === 'android') {
-      phoneNumber = 'tel:${16508881712}';
-    } else {
-      phoneNumber = 'telprompt:${16508881712}';
-    }
-    Linking.openURL(phoneNumber);
-  };
-
   useEffect(() => {
     getProfilePic();
     getDirections('32.946709, -96.952667', '32.951520, -96.955670');
@@ -143,10 +79,6 @@ const SpotExchange = () => {
     setCurrentlocation(coordinatesSeperator(user.matchedUsersData.body.currentLocation));
     setDesiredLocation(coordinatesSeperator(user.matchedUsersData.body.desiredLocation));
   }, []);
-
-  useEffect(() => {
-    retrieveTransactionChatRoomMessages();
-  }, [message]);
 
   return (
     <View style={styles.mainContainer}>
@@ -189,97 +121,25 @@ const SpotExchange = () => {
           </View>
         ) : null}
         {/* User hub and communication Modal */}
+
         {userHubModalVis ? (
-          <View style={styles.mainCommunicationsHub}>
-            <TouchableOpacity
-              style={styles.communicationHubCloseX}
-              onPress={() => setuserHubModalVis(false)}>
-              <Image style={styles.xImage} source={exit} />
-            </TouchableOpacity>
-            <Text style={styles.text}>Walter White</Text>
-            <Text style={styles.text}>BMW, 3 Series, Black</Text>
-            <Text style={styles.text}>FF35DG2</Text>
-            <View style={styles.starContainer}>
-              <Stars starSize={20} starWidth={3} rating={user.rating} />
-            </View>
-
-            <View style={styles.transactionsButtonContainer}>
-              <Button
-                size="small"
-                title="Cancel Transaction"
-                backgroundColor={theme.colors.light}
-                titleColor={theme.colors.error}
-                onPress={() => {
-                  setCancelCompleteModalVis({
-                    visible: !cancelCompleteModalVis.visible,
-                    type: 'cancelTransaction',
-                  });
-                  setuserHubModalVis(!userHubModalVis);
-                }}
-              />
-              <Button
-                title="Spot Switch complete"
-                size="small"
-                onPress={() => {
-                  setCancelCompleteModalVis({
-                    visible: !cancelCompleteModalVis.visible,
-                    type: 'spotSwitchComplete',
-                  });
-                  setuserHubModalVis(!userHubModalVis);
-                }}
-              />
-            </View>
-            <View style={styles.chatContainer}>
-              <ScrollView style={styles.scrollMessagingContainer}>
-                <View style={styles.scrollItemsContainer}>{displayMessages()}</View>
-              </ScrollView>
-              <View style={styles.messageChat}>
-                <TextInput
-                  style={styles.input}
-                  placeholder="Type a message..."
-                  onChangeText={(text) => setMessage(text)}
-                  value={message}
-                />
-                {message.length ? (
-                  <TouchableOpacity
-                    style={styles.sendMessageButton}
-                    onPress={pushTransactionChatRoomMessage}>
-                    <Image source={sendMessage} />
-                  </TouchableOpacity>
-                ) : null}
-              </View>
-
-              <View style={styles.buttonsContainer}>
-                <Button
-                  size="small"
-                  title="On My Way!"
-                  customButtonStyles={styles.button}
-                  customTextStyles={styles.buttonTitle}
-                  onPress={() => pushTransactionChatRoomMessage('On My Way!')}
-                />
-                <Button
-                  size="small"
-                  title="Almost There"
-                  customButtonStyles={styles.button}
-                  customTextStyles={styles.buttonTitle}
-                  onPress={() => pushTransactionChatRoomMessage('Almost There')}
-                />
-                <Button
-                  size="small"
-                  title="I'm Here"
-                  customButtonStyles={styles.button}
-                  customTextStyles={styles.buttonTitle}
-                  onPress={() => pushTransactionChatRoomMessage("I'm Here")}
-                />
-                <Button
-                  icon={phone}
-                  customButtonStyles={styles.button}
-                  customTextStyles={styles.buttonTitle}
-                  onPress={makeCall}
-                />
-              </View>
-            </View>
-          </View>
+          <HubModal
+            closeHub={() => setuserHubModalVis(!userHubModalVis)}
+            cancelPress={() => {
+              setCancelCompleteModalVis({
+                visible: !cancelCompleteModalVis.visible,
+                type: 'cancelTransaction',
+              });
+              setuserHubModalVis(!userHubModalVis);
+            }}
+            spotSwitchCompletePress={() => {
+              setCancelCompleteModalVis({
+                visible: !cancelCompleteModalVis.visible,
+                type: 'spotSwitchComplete',
+              });
+              setuserHubModalVis(!userHubModalVis);
+            }}
+          />
         ) : null}
 
         {/* Cancel or Complete Ride Modal */}
